857649fe8fe538754c8ad627ca82c39dced0f74b
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-21 09:20:37 +0600

    firebase install and add firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyCQ7VXeOIkBtPqTNFYYdZw5Ur7RVgOsmTA",
+   authDomain: "slack-v3.firebaseapp.com",
+   projectId: "slack-v3",
+   storageBucket: "slack-v3.appspot.com",
+   messagingSenderId: "361593187412",
+   appId: "1:361593187412:web:83aecd81e8dbdfce58d1c1",
+   measurementId: "G-YLPDSD1P62"
+};
