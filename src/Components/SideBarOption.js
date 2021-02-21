02118bb4c169b2e8c8930b05160cc868a34dd790
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-21 16:18:41 +0600

    set a conditon if there is no icon then show it and chose a color

--- src/Components/SideBarOption.js
+++ src/Components/SideBarOption.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 const SideBarOption = ({ Icon, Title }) => {
 
    return (
       <>
          <SideBarContainer className=''>
-            <span>{Icon && <Icon style={{ color: '#F2E2CE' }} className='mr-2' fontSize='small' />}</span> <p>{Title}</p>
+            <span>{Icon && <Icon style={{ color: '#F5FCDC' }} className='mr-2' fontSize='small' />}</span>
+            <p>
+               {
+                  Icon ? Title : <div className=""> <VisibilityIcon style={{ color: '#F5FCDC' }} className='mr-2' fontSize='small' /> {Title}</div>
+               }
+            </p>
 
          </SideBarContainer>
       </>
