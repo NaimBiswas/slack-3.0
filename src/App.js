3b1a5cea0754896bb02b6b9481c74975c1a66bec
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-21 10:17:08 +0600

    imstall material ui with icon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Home from './Components/Pages/Home';
+import Header from './Components/Header';
 function App() {
    return (
       <div className="App">
          <Router>
             <Switch>
                <Route path='/' exact>
-                  <Home></Home>
+                  <Header></Header>
                </Route>
             </Switch>
          </Router>
