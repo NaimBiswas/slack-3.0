c39ed959cf61657c006d7bf0ee24228230efeaef
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-21 21:34:12 +0600

    fix a issu

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -6,7 +6,7 @@ export const appSlice = createSlice({
       roomId: null,
    },
    reducers: {
-      enterRoom: (state, action) {
+      enterRoom: (state, action) => {
          state.roomId = action.payload.roomId
       }
    },
@@ -27,6 +27,6 @@ export const { enterRoom } = appSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectRoomId = state => state.roomId.name;
+export const selectRoomId = state => state.appReducer.roomId;
 
 export default appSlice.reducer;
