6472bfb73e2f909dfd26a92a8d2a80b41d4ccc52
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-21 10:56:37 +0600

    add icons with the help of sytle-components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Home from './Components/Pages/Home';
 import Header from './Components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
    return (
       <div className="App">
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,35 @@
 
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
+import { Avatar } from '@material-ui/core'
+import { AccessTime, Copyright } from '@material-ui/icons'
+import HelpIcon from '@material-ui/icons/Help';
 
 const Header = () => {
    return (
       <Fragment>
          <HeaderComponent>
-            <h2> Header Components 😁</h2>
+            <HeaderLeft>
+
+            </HeaderLeft>
+            <HeaderMiddle>
+               <div className="">
+                  <HeaderRightTime />
+               </div>
+               <input className='form-control' type="text" />
+               <div className="">
+                  <HeaderRightHelp />
+               </div>
+            </HeaderMiddle>
+            <HeaderRight>
+               <HeaderRightAvatar
+
+
+               />
+
+
+
+            </HeaderRight>
          </HeaderComponent>
 
       </Fragment>
@@ -15,7 +38,22 @@ const Header = () => {
 
 export default Header
 
+const HeaderRightAvatar = styled(Avatar)`
+`
+const HeaderRightTime = styled(AccessTime)``
+const HeaderRightHelp = styled(HelpIcon)``
+
 const HeaderComponent = styled.div`
    background-color:red;
    color:white;
+   justify-content: space-between;
+   display: flex;
+`
+const HeaderLeft = styled.div``
+const HeaderMiddle = styled.div`
+display:flex;
+justify-content: space-around
+`
+const HeaderRight = styled.span`
+
 `
