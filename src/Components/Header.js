c495393a2c2a24f578dc930192f8904c5f7e859a
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-21 11:27:41 +0600

    make header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,8 @@ const Header = () => {
                <div className="">
                   <HeaderRightTime />
                </div>
-               <input className='form-control' type="text" />
+               <HeaderMiddleInput placeholder='Search' className='pl-4' />
+               {/* <input size='90' className='form-control' type="text" /> */}
                <div className="">
                   <HeaderRightHelp />
                </div>
@@ -26,11 +27,9 @@ const Header = () => {
 
 
                />
-
-
-
             </HeaderRight>
          </HeaderComponent>
+         <HeaderBOttonBorder></HeaderBOttonBorder>
 
       </Fragment>
    )
@@ -39,12 +38,27 @@ const Header = () => {
 export default Header
 
 const HeaderRightAvatar = styled(Avatar)`
+margin-right:30px;
+:hover{
+   opacity:50%;
+   cursor: pointer;
+}
 `
-const HeaderRightTime = styled(AccessTime)``
-const HeaderRightHelp = styled(HelpIcon)``
+const HeaderRightTime = styled(AccessTime)`
+margin-right:23px;
+margin-top:8px;
+`
+const HeaderRightHelp = styled(HelpIcon)`
+margin-left:23px;
+margin-top:8px;
+`
+const HeaderBOttonBorder = styled.hr`
+margin: 0;
+background:red;
+`
 
 const HeaderComponent = styled.div`
-   background-color:red;
+   background-color:#0F1A1E;
    color:white;
    justify-content: space-between;
    display: flex;
@@ -52,8 +66,19 @@ const HeaderComponent = styled.div`
 const HeaderLeft = styled.div``
 const HeaderMiddle = styled.div`
 display:flex;
-justify-content: space-around
+justify-content: space-around;
 `
+const HeaderMiddleInput = styled.input`
+    background: #181717;
+    border: 1px solid #3d3d3d;
+    border-radius: 999px;
+    color:#F2E2CE;
+    
+    :focus{
+       outline:none;
+    }
+`
+
 const HeaderRight = styled.span`
 
 `
